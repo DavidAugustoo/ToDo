c7848a9b7f4116946eb7c6bd53e82794dc7855db
David Augusto <davidribeiro086@gmail.com>
2022-07-17 20:20:09 -0300

    style: input and task button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Header } from './components/Header/Header'
-import './global.css'
+import { Header } from './components/Header/Header';
+import { PlusCircle } from 'phosphor-react';
+
+import style from './App.module.css';
+
+import './global.css';
 
 function App() {
   return (
+    <>
     <Header />
+
+    <form className={style.taskForm} action="">
+      <input placeholder="Adicione uma nova tarefa" />
+
+      <button type="submit">
+        Criar
+        <PlusCircle size={20} weight="bold"/>
+      </button>
+    </form>
+
+    </>
   )
 }
 
